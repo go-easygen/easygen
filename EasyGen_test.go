d39bae23cdde994c66512c6f5cda6c850c685c12
Tong Sun <suntong@cpan.org>
2015-06-28 15:39:28 -0400

    - [+] add 2nd test case, with 2 cases

--- EasyGen_test.go
+++ EasyGen_test.go
@@ -8,6 +8,22 @@ func TestList0(t *testing.T) {
 	t.Log("First and plainest list test")
 	const await = "The colors are: red, blue, white, .\n"
 	if got := Generate(false, "list0"); got != await {
-		t.Errorf("Mismatch:, got %s instead", got)
+		t.Errorf("Mismatch:, got '%s' instead", got)
+	}
+}
+
+func TestList1Text(t *testing.T) {
+	t.Log("Second test, with text template")
+	const await = "The quoted colors are: \"red\", \"blue\", \"white\", .\n"
+	if got := Generate(false, "list1"); got != await {
+		t.Errorf("Mismatch:, got '%s' instead", got)
+	}
+}
+
+func TestList1HTML(t *testing.T) {
+	t.Log("Second test, with html template")
+	const await = "The quoted colors are: &#34;red&#34;, &#34;blue&#34;, &#34;white&#34;, .\n"
+	if got := Generate(true, "list1"); got != await {
+		t.Errorf("Mismatch:, got '%s' instead", got)
 	}
 }
