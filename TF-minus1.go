f6f5f922e72b6ca15d026145d2df87b2852a003b
Tong Sun <suntong@cpan.org>
2015-07-01 18:27:11 -0400

    - [+] add credit due

--- TF-minus1.go
+++ TF-minus1.go
@@ -3,5 +3,8 @@ package main
 //==========================================================================
 // template function
 
+// By Caleb Spare @gmail.com
+// https://groups.google.com/d/msg/golang-nuts/gzAyBLAeUbU/LwgomgxcjQ8J
+
 // Get input less 1
 func minus1(n int) int { return n - 1 }
