19b90fcc2183fade2b71972bb595a5c60b2e91e5
Tong Sun <suntong@cpan.org>
2017-07-09 17:21:48 -0400

    - [*] remove v1 funcs and switch all to v2

--- easygen.go
+++ easygen.go
@@ -18,14 +18,12 @@ Many examples have been provided to showcase its functionality, and different wa
 package easygen
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
-	tt "text/template"
 
 	"gopkg.in/yaml.v2"
 )
@@ -44,106 +42,6 @@ var Opts = Options{ExtYaml: ".yaml", ExtTmpl: ".tmpl"}
 
 ////////////////////////////////////////////////////////////////////////////
 // Function definitions
-
-// Generate2 will produce output according to the given template and driving data files,
-// specified via fileNameTempl and fileNames (for data) respectively.
-func Generate2(HTML bool, fileNameTempl string, fileNames ...string) (ret string) {
-	Opts.TemplateFile = fileNameTempl
-
-	for _, fileName := range fileNames {
-		ret += Generate(HTML, fileName)
-	}
-	Opts.TemplateFile = ""
-	return ret
-}
-
-// Generate0 will produce output according from driving data without a template file, using the string from strTempl as the template
-func Generate0(HTML bool, strTempl string, fileName string) string {
-	Opts.TemplateStr = strTempl
-	ret := Generate(HTML, fileName)
-	Opts.TemplateStr = ""
-	return ret
-}
-
-// Generate will produce output from the template according to the corresponding driving data, fileName is for both template and data file name
-func Generate(HTML bool, fileName string) string {
-	var templates []string
-
-	// Allow to use fileName with and without the @Opts.ExtYaml suffix.
-	// If both <file>.yaml and <file> exist, prefer <file>.yaml.
-	if _, err := os.Stat(fileName + Opts.ExtYaml); err == nil {
-		fileName += Opts.ExtYaml
-	} else if _, err = os.Stat(fileName); os.IsNotExist(err) {
-		fileName += Opts.ExtYaml
-	}
-
-	// Allow to use @Opts.TemplateFile without the @Opts.ExtTmpl suffix.
-	if len(Opts.TemplateFile) > 0 {
-		for _, template := range strings.Split(Opts.TemplateFile, ",") {
-			if _, err := os.Stat(template); os.IsNotExist(err) {
-				template += Opts.ExtTmpl
-			}
-			templates = append(templates, template)
-		}
-	} else if idx := strings.LastIndex(fileName, "."); idx > 0 {
-		templates = []string{fileName[:idx] + Opts.ExtTmpl}
-	} else {
-		templates = []string{fileName + Opts.ExtTmpl}
-	}
-
-	m := ReadYamlFile(fileName)
-
-	env := make(map[string]string)
-	for _, e := range os.Environ() {
-		sep := strings.Index(e, "=")
-		env[e[0:sep]] = e[sep+1:]
-	}
-	m["ENV"] = env
-	//fmt.Printf("] %+v\n", m)
-
-	buf := new(bytes.Buffer)
-	for _, template := range templates {
-		t, err := ParseFiles(HTML, template)
-		checkError(err)
-
-		err = t.Execute(buf, m)
-		checkError(err)
-	}
-	return buf.String()
-}
-
-// ParseFiles wraps parsing text or HTML template files into a single
-// function, dictated by the first parameter "HTML".
-// By Matt Harden @gmail.com
-func ParseFiles(HTML bool, filenames ...string) (Template, error) {
-	var tname string
-
-	if len(Opts.TemplateStr) > 0 {
-		tname = "TT"
-	} else if len(filenames) == 0 {
-		return nil, fmt.Errorf("ParseFiles called without template filename")
-	} else {
-		tname = filepath.Base(filenames[0])
-	}
-
-	textTemplate := tt.New(tname).Funcs(tt.FuncMap{
-		"eqf":      strings.EqualFold,
-		"split":    strings.Fields,
-		"minus1":   minus1,
-		"dateI":    dateI,
-		"year4":    year4,
-		"replace":  replace,
-		"replacec": replacec,
-	})
-
-	if len(Opts.TemplateStr) > 0 {
-		return textTemplate.Parse(Opts.TemplateStr)
-	}
-	return textTemplate.ParseFiles(filenames...)
-}
-
-////////////////////////////////////////////////////////////////////////////
-// Version 2 Function definitions
 
 // ReadDataFile reads in the driving data from the given file, which can
 // be optionally without the defined extension
--- easygen_test.go
+++ easygen_test.go
@@ -1,232 +1,64 @@
 package easygen_test
 
 import (
-	"fmt"
 	"os"
-	"testing"
+	"text/template"
 
 	"github.com/go-easygen/easygen"
 )
 
+var tmpl *template.Template
+
+func init() {
+	tmpl = easygen.NewTemplate().Funcs(easygen.FuncDefs())
+}
+
 ////////////////////////////////////////////////////////////////////////////
 // Lists
-
-//==========================================================================
-// list0
-
-func TestList0(t *testing.T) {
-	t.Log("First and plainest list test")
-	const await = "The colors are: red, blue, white, .\n"
-	if got := easygen.Generate(false, "test/list0"); got != await {
-		t.Errorf("Mismatch:, got '%s' instead", got)
-	}
-}
-
-//==========================================================================
-// list0 data + Env var
-
-// Test Env variable with list0 data
-func ExampleList0_Env() {
-	// Equivalent testing on commandline:
-	//   easygen -tf test/list0E test/list0
-	fmt.Println(os.Getenv("SHELL"))
-	fmt.Print(easygen.Generate2(false, "test/list0E", "test/list0"))
-	// Output:
-	// /bin/bash
-	// The colors are: red, blue, white, .
-	// The system shell is: /bin/bash
-	// Different shells are: /bin/bash-red, /bin/bash-blue, /bin/bash-white,
-}
-
-//==========================================================================
-// list1
-
-func TestList1Text(t *testing.T) {
-	t.Log("Second test, with text template")
-	const await = "The quoted colors are: \"red\", \"blue\", \"white\", .\n"
-	if got := easygen.Generate(false, "test/list1"); got != await {
-		t.Errorf("Mismatch:, got '%s' instead", got)
-	}
-}
-
-//==========================================================================
-// listfunc1
-
-func TestListFunc1(t *testing.T) {
-	t.Log("Test custom template function - minus1")
-	const await = "red, blue, white.\n"
-	if got := easygen.Generate(false, "test/listfunc1"); got != await {
-		t.Errorf("Mismatch:, got '%s' instead", got)
-	}
-}
-
-// Test the provided listfunc1, template and data
-func ExampleGenerate_listfunc1() {
-	// Equivalent testing on commandline:
-	//   easygen test/listfunc1
-	fmt.Print(easygen.Generate(false, "test/listfunc1"))
-	// Output:
-	// red, blue, white.
-}
-
-//==========================================================================
-// list0 data + listfunc1 template
-// I.e.: EasyGen -tf test/listfunc1 test/list0
-
-// Test the provided listfunc1 template with list0 data
-func ExampleGenerate2_listfunc1List0() {
-	// Equivalent testing on commandline:
-	//   easygen -tf test/listfunc1 test/list0
-	fmt.Print(easygen.Generate2(false, "test/listfunc1", "test/list0"))
-	// Output:
-	// red, blue, white.
-}
 
 //==========================================================================
 // list0 data + string template
 // I.e.: EasyGen -ts "{{range .Colors}}{{.}}, {{end}}" test/list0
 
 // Test string template with list0 data
-func ExampleGenerate0_list0StrTemplate() {
+func ExampleProcess0_list0StrTemplate() {
 	// Equivalent testing on commandline:
 	//   easygen -ts '{{range .Colors}}{{.}}, {{end}}' test/list0
-	fmt.Print(easygen.Generate0(false, "{{range .Colors}}{{.}}, {{end}}", "test/list0"))
+	easygen.Process0(tmpl, os.Stdout,
+		"{{range .Colors}}{{.}}, {{end}}", "test/list0")
 	// Output:
 	// red, blue, white,
-}
-
-////////////////////////////////////////////////////////////////////////////
-// Commandline definitions
-
-func ExampleTestExample() {
-	// Ref https://groups.google.com/d/msg/golang-nuts/9jKexxD19Js/xrNAQA1ACnMJ
-	fmt.Println(`  flags.Bool("Debug", false, "Turn on debugging.")`)
-	fmt.Println(`  viper.BindPFlag("Debug", flags.Lookup("Debug"))`)
-	// Output:
-	//   flags.Bool("Debug", false, "Turn on debugging.")
-	//   viper.BindPFlag("Debug", flags.Lookup("Debug"))
-}
-
-//   easygen test/commandlineCV | sed 's|^\t|&//&|; s|^$|\t//|'
-
-// Test the provided Cobra/Viper command line flag auto generation
-func ExampleGenerate_commandLineCobraViper() {
-	// Equivalent testing on commandline:
-	//   easygen test/commandlineCV
-	fmt.Print(easygen.Generate(false, "test/commandlineCV"))
-	// Output:
-	//
-	//	flags.Bool("debug", false, "Turn on debugging.")
-	//	viper.BindPFlag("debug", flags.Lookup("debug"))
-	//
-	//	flags.String("addr", "localhost:5002", "Address of the service.")
-	//	viper.BindPFlag("addr", flags.Lookup("addr"))
-	//
-	//	flags.String("smtp-addr", "localhost:25", "Address of the SMTP server.")
-	//	viper.BindPFlag("smtp-addr", flags.Lookup("smtp-addr"))
-	//
-	//	flags.String("smtp-user", "", "User for the SMTP server.")
-	//	viper.BindPFlag("smtp-user", flags.Lookup("smtp-user"))
-	//
-	//	flags.String("smtp-password", "", "Password for the SMTP server.")
-	//	viper.BindPFlag("smtp-password", flags.Lookup("smtp-password"))
-	//
-	//	flags.String("email-from", "noreply@abc.com", "The from email address.")
-	//	viper.BindPFlag("email-from", flags.Lookup("email-from"))
-	//
-}
-
-//   easygen test/commandlineCVFull | sed 's|^|\t// |;'
-
-// Test the provided Cobra/Viper command line flag init() function auto generation
-func ExampleGenerate_commandLineCobraViperInit() {
-	// Equivalent testing on commandline:
-	//   easygen test/commandlineCVFull
-	fmt.Print(easygen.Generate(false, "test/commandlineCVFull"))
-	// Output:
-	// func init() {
-	//
-	//   viper.SetEnvPrefix("DISPATCH")
-	//   viper.AutomaticEnv()
-	//
-	//   /*
-	//
-	//     When AutomaticEnv called, Viper will check for an environment variable any
-	//     time a viper.Get request is made. It will apply the following rules. It
-	//     will check for a environment variable with a name matching the key
-	//     uppercased and prefixed with the EnvPrefix if set.
-	//
-	//   */
-	//
-	//   flags := mainCmd.Flags()
-	//
-	//   flags.Bool("Debug", false, "Turn on debugging.")
-	//   viper.BindPFlag("Debug", flags.Lookup("Debug"))
-	//
-	//   flags.String("addr", "localhost:5002", "Address of the service.")
-	//   viper.BindPFlag("addr", flags.Lookup("addr"))
-	//
-	//   flags.String("smtp-addr", "localhost:25", "Address of the SMTP server.")
-	//   viper.BindPFlag("smtp-addr", flags.Lookup("smtp-addr"))
-	//
-	//   flags.String("smtp-user", "", "User for the SMTP server.")
-	//   viper.BindPFlag("smtp-user", flags.Lookup("smtp-user"))
-	//
-	//   flags.String("smtp-password", "", "Password for the SMTP server.")
-	//   viper.BindPFlag("smtp-password", flags.Lookup("smtp-password"))
-	//
-	//   flags.String("email-from", "noreply@abc.com", "The from email address.")
-	//   viper.BindPFlag("email-from", flags.Lookup("email-from"))
-	//
-	//   // Viper supports reading from yaml, toml and/or json files. Viper can
-	//   // search multiple paths. Paths will be searched in the order they are
-	//   // provided. Searches stopped once Config File found.
-	//
-	//   viper.SetConfigName("CommandLineCV") // name of config file (without extension)
-	//
-	//   viper.AddConfigPath("/tmp")
-	//   viper.AddConfigPath(".")
-	//
-	//   err := viper.ReadInConfig()
-	//   if err != nil {
-	//     println("No config file found. Using built-in defaults.")
-	//   }
-	//
-	// }
-}
-
-////////////////////////////////////////////////////////////////////////////
-// Varible Names
-
-func ExampleVaribleNames() {
 }
 
 ////////////////////////////////////////////////////////////////////////////
 // Strings Test
 
 // Test string comparison in template
-func ExampleGenerate0_stringsCmp() {
+func ExampleProcess0_stringsCmp() {
 	// Equivalent testing on commandline:
 	//   easygen -ts '{{The {{if ... {{end}}.' test/strings0
-	fmt.Print(easygen.Generate0(false, `The {{if eq .StrTest "-AB-axxb- HTML Html html"}}eq says Yea{{else}}eq says Nay{{end}} but {{if eqf .StrTest "-AB-axxb- HTML Html html"}}eqf says Yea{{else}}eqf says Nay{{end}}.`, "test/strings0"))
+	easygen.Process0(tmpl, os.Stdout,
+		`The {{if eq .StrTest "-AB-axxb- HTML Html html"}}eq says Yea{{else}}eq says Nay{{end}} but {{if eqf .StrTest "-AB-axxb- HTML Html html"}}eqf says Yea{{else}}eqf says Nay{{end}}.`, "test/strings0")
 	// Output:
 	// The eq says Nay but eqf says Yea.
 }
 
 // Test the string split function in template
-func ExampleGenerate0_split0() {
+func ExampleProcess0_split0() {
 	// Equivalent testing on commandline:
 	//   easygen -ts '{{split .Colorlist}}' test/list0
-	fmt.Print(easygen.Generate0(false, `{{split .Colorlist}}`, "test/list0"))
+	easygen.Process0(tmpl, os.Stdout,
+		`{{split .Colorlist}}`, "test/list0")
 	// Output:
 	// [red blue white]
 }
 
 // Test the string split function in template again
-func ExampleGenerate0_split1() {
+func ExampleProcess0_split1() {
 	// Equivalent testing on commandline:
 	//   easygen -ts '{{range ... {{end}}' test/list0
-	fmt.Print(easygen.Generate0(false, `{{range (split .Colorlist)}}{{.}} {{end}}`, "test/list0"))
+	easygen.Process0(tmpl, os.Stdout,
+		`{{range (split .Colorlist)}}{{.}} {{end}}`, "test/list0")
 	// Output:
 	// red blue white
 }
