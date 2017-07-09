cb6fb5ae3444c9b66ae6908d158e6d1b0e7ed30e
Tong Sun <suntong@cpan.org>
2017-07-09 15:19:09 -0400

    - [*] change how config.go is built, because of https://groups.google.com/d/msg/golang-nuts/zHHhNlRjYvE/8IOih4ILBgAJ

--- cmd/easygen/easygen.gen.sh
+++ cmd/easygen/easygen.gen.sh
@@ -1,2 +1,2 @@
- easygen -tf config easygen | gofmt > config.go-
+ easygen -tf config easygen | gofmt > ../../config.go
  easygen -tf flags easygen | sed 's/^package easygen/package main/' | gofmt > flags.go
