5fd01cec1c3c91071bab6a124da100b7151510fd
Tong Sun <suntong@cpan.org>
2019-06-05 10:23:39 -0400

    - [+] add more demo cases

--- example_execute_test.go
+++ example_execute_test.go
@@ -1,7 +1,9 @@
 package easygen_test
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/go-easygen/easygen"
 	"github.com/go-easygen/easygen/egCal"
@@ -15,21 +17,32 @@ type variable struct {
 // for standalone test, change package to `main` and the next func def to,
 // func main() {
 func ExampleExecute() {
+	easygen.Opts.Debug = 1
+
 	tmpl0 := easygen.NewTemplate().Customize()
 	tmpl := tmpl0.Funcs(easygen.FuncDefs()).Funcs(egVar.FuncDefs()).Funcs(egCal.FuncDefs())
 
 	// define driving data of any tye
 	v0 := variable{"some-init-method"}
-	easygen.Opts.Debug = 1
-
 	// https://godoc.org/github.com/go-easygen/easygen#Execute
 	// provide full template file name with extension
 	easygen.Execute(tmpl, os.Stdout, "test/var0.tmpl", v0)
+
+	// Demo of using driving data of pure slice/array
+	v1 := []string{"red", "blue", "white"}
+	easygen.Execute(tmpl, os.Stdout, "test/list00.tmpl", v1)
+
+	// Demo output to string
+	var b strings.Builder
+	easygen.Execute(tmpl, &b, "test/list00f.tmpl", v1)
+	fmt.Print(b.String())
 
 	// Output:
 	// Input: "some-init-method"
 	// Output 1: "SomeInitMethod"
 	// Output 2: "SOME_INIT_METHOD"
+	// The colors are: red, blue, white, .
+	// The colors are: red, blue, white.
 }
 
 // To show the full code in GoDoc
